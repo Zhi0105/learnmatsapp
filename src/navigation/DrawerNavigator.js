319f33e4b87b483b21014ca5b30a74eb974f5e81
Argie <argie.b@flowerstore.ph>
2024-03-09 17:40:40 +0800

    updates

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import { HomeScreen } from '@_screens/Account/HomeScreen';
 import { SettingsScreen } from '@_screens/Account/SettingsScreen';
 import { LogoutScreen } from '@_screens/Account/LogoutScreen';
+
+import { HomeStack } from './Stack/HomeStack';
 
 export const DrawerNavigator = () => {
   const Drawer = createDrawerNavigator();
 
   return (
     <Drawer.Navigator
-      initialRouteName='Home'
+      initialRouteName='HomeStack'
       screenOptions={{
         headerTitle: 'Alabang Elementary School',
         headerStyle: {
@@ -20,7 +21,7 @@ export const DrawerNavigator = () => {
         headerTitleAlign: "center"
       }}
     >
-      <Drawer.Screen name="Home"  component={HomeScreen} />
+      <Drawer.Screen name="HomeStack"  component={HomeStack} />
       <Drawer.Screen name="Settings" component={SettingsScreen} />
       <Drawer.Screen name="Signout" component={LogoutScreen} />
     </Drawer.Navigator>
